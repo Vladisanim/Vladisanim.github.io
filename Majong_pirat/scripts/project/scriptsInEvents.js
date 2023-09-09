14cb878b50acfe576f995966afa24af472d3d8cf
Vladisanim <99267611+Vladisanim@users.noreply.github.com>
2023-09-09 14:41:49 +0400

    Add files via upload

---
+++ Majong_pirat/scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+
+
+
+const scriptsInEvents = {
+
+	async Eload_Event1_Act2(runtime, localVars)
+	{
+		runtime.globalVars.currentDate = new Date(runtime.GameScore.serverTime).toLocaleString('en', {
+		  year: 'numeric',
+		  month: '2-digit',
+		  day: '2-digit',
+		});
+		console.log({  currentDate: runtime.globalVars.currentDate });
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
