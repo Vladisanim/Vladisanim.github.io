872fc243b12570d5dffa6a44ed1a0c760111c90c
Vladisanim <99267611+Vladisanim@users.noreply.github.com>
2024-03-18 19:26:40 +0400

    Add files via upload

---
+++ TouchAnalogTutorial1/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1710775464,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/fanapad-sheet0.png",
+		"images/fanarangebox-sheet0.png",
+		"images/fanastick-sheet0.png",
+		"images/arm-sheet0.png",
+		"images/player_body-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/solid-sheet0.png",
+		"images/hud_arrowswalk-sheet0.png",
+		"images/hud_arrowswalk-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"loading-logo.png",
+		"icon-256.png"
+	]
+}
