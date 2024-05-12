a362590726b42f1a13436d4a1d30886a2fb232e7
Vladisanim <99267611+Vladisanim@users.noreply.github.com>
2024-05-12 17:03:19 +0400

    Add files via upload

---
+++ DrawtosaveStickman1/scripts/project/scriptsInEvents.js
@@ -0,0 +1,23 @@
+import * as Con from "./confetti.browser.min.js";
+
+
+const scriptsInEvents = {
+
+	async E_level_Event115(runtime, localVars)
+	{
+		function randomInRange(min, max) {
+		  return Math.random() * (max - min) + min;
+		}
+		
+		confetti({
+		  angle: randomInRange(55, 125),
+		  spread: randomInRange(50, 70),
+		  particleCount: randomInRange(50, 100),
+		  origin: { x: 1/runtime.layout.getLayer(0).getViewport().width * runtime.objects.Confetty.getFirstInstance().x, y: 1/runtime.layout.getLayer(0).getViewport().height * runtime.objects.Confetty.getFirstInstance().y }
+		});
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
